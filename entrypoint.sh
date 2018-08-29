554a20b4e3ae13f76ec483ec2513fa99c8b33241
sstutanotam <40928227+sstutanotam@users.noreply.github.com>
2018-08-29 22:43:53 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+if [ ! -d /caddybin/caddy_v$CADDY_VER ]; then
+  rm -rf /caddybin
+  mkdir /caddybin
+  mkdir /caddybin/caddy_v$CADDY_VER
+  cd /caddybin/caddy_v$CADDY_VER
+  wget -O caddy.tar.gz 'https://caddyserver.com/download/linux/amd64?plugins=http.forwardproxy&license=personal&telemetry=off'
+  tar xvf caddy.tar.gz 
+  chmod +x caddy
+fi
+
+if [ ! -d $CADDY_ROOT ]; then
+  mkdir $CADDY_ROOT
+  cd $CADDY_ROOT
+  wget -O wallet.bitshares.org-gh-pages.zip https://github.com/bitshares/wallet.bitshares.org/archive/gh-pages.zip
+  unzip wallet.bitshares.org-gh-pages.zip
+fi
+
+cd /caddybin/caddy_v$CADDY_VER
+echo $CADDY_LS_IP:$PORT { > HerokuCaddyfile
+echo root $CADDY_ROOT/wallet.bitshares.org-gh-pages >> HerokuCaddyfile
+echo gzip >> HerokuCaddyfile
+echo index $CADDY_INDEX >> HerokuCaddyfile
+echo forwardproxy { >> HerokuCaddyfile
+echo basicauth $CADDY_USER $CADDY_PASS >> HerokuCaddyfile
+echo hide_ip >> HerokuCaddyfile
+echo hide_via >> HerokuCaddyfile
+echo } >> HerokuCaddyfile
+echo } >> HerokuCaddyfile
+./caddy -conf="HerokuCaddyfile"
